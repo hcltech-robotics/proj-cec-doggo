41605e19b3622d682d05e79285b7526b87363a8a
Mátyás Budavári <budavariam@gmail.com>
2024-12-11 15:22:32 -0600

    add ws

--- components/vite-threejs/src/App.tsx
+++ components/vite-threejs/src/App.tsx
@@ -1,11 +1,15 @@
 import { useEffect } from "react";
 import { init, animate } from "./scene";
+import { main } from "./Websocket";
 
 const App = () => {
   useEffect(() => {
     init()
     animate()
 
+  }, [])
+  useEffect(() => {
+    main()
   }, [])
   return null;
 };
---
+++ components/vite-threejs/src/Websocket.tsx
@@ -0,0 +1,37 @@
+import { FoxgloveClient } from "@foxglove/ws-protocol";
+//import { WebSocket } from "ws";
+import { MessageReader } from "@foxglove/rosmsg2-serialization";
+import { parse, stringify } from "@foxglove/rosmsg";
+
+async function main() {
+    const client = new FoxgloveClient({
+        ws: new WebSocket(`ws://localhost:8765`, [FoxgloveClient.SUPPORTED_SUBPROTOCOL]),
+    });
+    const deserializers = new Map();
+    client.on("advertise", (channels) => {
+        for (const channel of channels) {
+            if (channel.encoding === "json") {
+                const subId = client.subscribe(channel.id);
+                const textDecoder = new TextDecoder();
+                deserializers.set(subId, (data: any) => JSON.parse(textDecoder.decode(data)));
+            } else if (channel.encoding === "cdr") {
+                debugger
+                // message definition comes from `parse()` in @foxglove/rosmsg
+                //const reader = new MessageReader(messageDefinition);
+
+            } else {
+                console.warn(`Unsupported encoding ${channel.encoding}`);
+                continue;
+            }
+        }
+    });
+    client.on("message", ({ subscriptionId, timestamp, data }) => {
+        console.log({
+            subscriptionId,
+            timestamp,
+            data: deserializers.get(subscriptionId)(data),
+        });
+    });
+}
+
+export { main }
