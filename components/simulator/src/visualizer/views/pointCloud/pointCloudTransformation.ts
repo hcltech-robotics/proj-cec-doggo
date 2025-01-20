d0dddadea36a6da2f1c483a4606edd6de4f0ddd0
Istvan Ivanyi <ivanyii@starschema.net>
2025-01-20 16:12:51 +0100

    cleanup pointcloud transformations + add types file

--- components/simulator/src/visualizer/views/pointCloud/pointCloudTransformation.ts
+++ components/simulator/src/visualizer/views/pointCloud/pointCloudTransformation.ts
@@ -1,43 +1,20 @@
 import { BufferGeometry, Color, Float32BufferAttribute, Points, PointsMaterial } from "three";
 import { SceneManager } from "../../SceneManager";
-
-interface PointCloudData {
-    data: Uint8Array;
-    point_step: number;
-    fields: { name: string, offset: Uint8Array }[];
-    is_bigendian: boolean;
-}
+import { PointCloudData, FieldsMap } from './pointCloudTypes';
 
 const parsePointCloud = ({ data, point_step, fields, is_bigendian }: PointCloudData) => {
     const points = [];
     const intensity = [];
-    const fieldsMap = fields.reduce((acc, { name, ...rest }) => {
-        acc[name] = { ...rest };
-        return acc;
-    }, {} as { [key: string]: any });
     const uint8ArrayData = new Uint8Array(data);
+    const fieldsMap: FieldsMap = fields.reduce((acc, { name, ...rest }) => {
+        acc[name as keyof FieldsMap] = { ...rest };
+        return acc;
+    }, {} as FieldsMap);
 
     for (let i = 0; i < data.length; i += point_step) {
         const x = new DataView(uint8ArrayData.buffer, i + fieldsMap['x'].offset, 4).getFloat32(0, !is_bigendian);
         const y = new DataView(uint8ArrayData.buffer, i + fieldsMap['y'].offset, 4).getFloat32(0, !is_bigendian);
         const z = new DataView(uint8ArrayData.buffer, i + fieldsMap['z'].offset, 4).getFloat32(0, !is_bigendian);
-
-        // console.log(`Point ${i / point_step}: x=${x}, y=${y}, z=${z}`, data[i], data[i + 1], data[i + 2], data[i + 3]);
-
-        // if (isNaN(x) || isNaN(y) || isNaN(z) || !isFinite(x) || !isFinite(y) || !isFinite(z)) {
-        //   console.log('x', data.buffer, i, 4);
-        //   console.log('y', data.buffer, i + 4, 4);
-        //   console.log('z', data.buffer, i + 8, 4);
-        //   console.log(new DataView(data.buffer, i, 4).getFloat32(0, true));
-        //   console.warn(`Hibás adat a(z) ${i / point_step}. pontnál: x=${x}, y=${y}, z=${z}`);
-        //   console.log(new DataView(data.buffer, i, 4).getFloat32(0, true));
-        //   console.log("Raw data segment:", data.slice(i, i + point_step));
-        //   continue;
-        // }
-
-        // if (data.length % point_step !== 0) {
-        //   console.warn("Adathossz nem osztható a point_step értékkel.");
-        // }
 
         if (!isNaN(x) && !isNaN(y) && !isNaN(z)) {
             points.push(x, y, z);
@@ -45,7 +22,6 @@ const parsePointCloud = ({ data, point_step, fields, is_bigendian }: PointCloudD
         }
     }
 
-    // console.log(points);
     return {
         points: new Float32Array(points),
         intensity: new Float32Array(intensity),
@@ -56,7 +32,6 @@ let pointsCloud: Points | null = null
 let pointCloudGeometry: BufferGeometry | null = null
 
 function updatePointCloud(s: SceneManager, g: any) {
-    // console.log(g);
     if (pointsCloud) {
         if (pointCloudGeometry) { pointCloudGeometry.dispose() }
         s.scenes.pointcloud.remove(pointsCloud)
@@ -67,7 +42,6 @@ function updatePointCloud(s: SceneManager, g: any) {
     pointCloudGeometry = new BufferGeometry();
     pointCloudGeometry.setAttribute("position", new Float32BufferAttribute(positions.points, 3));
     pointCloudGeometry.setAttribute("intensity", new Float32BufferAttribute(positions.intensity, 1));
-    // geometry.rotateX(Math.PI);
 
     const material = new PointsMaterial({
         size: 0.1,
---
+++ components/simulator/src/visualizer/views/pointCloud/pointCloudTypes.ts
@@ -0,0 +1,28 @@
+export interface PointCloudDataFieldValues {
+  name: string;
+  count: number;
+  datatype: number;
+  offset: number;
+}
+
+export interface PointCloudData {
+  data: Uint8Array;
+  point_step: number;
+  fields: PointCloudDataFieldValues[];
+  is_bigendian: boolean;
+}
+
+export interface FieldsMapFieldValues {
+  count: number;
+  datatype: number;
+  offset: number;
+}
+
+export interface FieldsMap {
+  x: FieldsMapFieldValues;
+  y: FieldsMapFieldValues;
+  z: FieldsMapFieldValues;
+  time: FieldsMapFieldValues;
+  ring: FieldsMapFieldValues;
+  intensity: FieldsMapFieldValues;
+}
