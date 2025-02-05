87f6d10b90d4346513e8bc1596468b6845d101d5
Csaba SZELL <szellcs@starschema.net>
2025-02-05 14:46:49 +0100

    Comment done actions

--- components/simulator/src/robot/playground.ts
+++ components/simulator/src/robot/playground.ts
@@ -1,27 +1,25 @@
 import { getChannelData, getClient } from "./foxgloveConnection";
 
 window.debugChannelData = () =>
-    Object.values(getChannelData()).map((e) => ({
-        sn: e.schemaName,
-        t: e.topic,
-        channel: e,
-    }));
+  Object.values(getChannelData()).map((e) => ({
+    sn: e.schemaName,
+    t: e.topic,
+    channel: e,
+  }));
 
 window.send_message = (txt: string) => {
-    //ros2 topic echo /chatter
-    const client = getClient()
-    if (!client) {
-        console.error("Foxglove client is not available")
-        return
-    }
-    const channelId = client.advertise({
-        topic: "/chatter",
-        encoding: "json",
-        schemaName: "std_msgs/String",
-    });
+  //ros2 topic echo /chatter
+  const client = getClient();
+  if (!client) {
+    console.error("Foxglove client is not available");
+    return;
+  }
+  const channelId = client.advertise({
+    topic: "/chatter",
+    encoding: "json",
+    schemaName: "std_msgs/String",
+  });
 
-    const message = new Uint8Array(
-        new TextEncoder().encode(JSON.stringify({ data: txt }))
-    );
-    client.sendMessage(channelId, message);
-}
+  const message = new Uint8Array(new TextEncoder().encode(JSON.stringify({ data: txt })));
+  client.sendMessage(channelId, message);
+};
