31c09ef35e3e16e400e9c3f2a063f113a9dd32e4
deejayy <deejayy@deejayy.hu>
2025-02-12 16:16:45 +0100

    Make a scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,50 @@
-import { Grid, OrbitControls, Stats } from '@react-three/drei';
+import { Environment, Grid, OrbitControls, Stats } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { Bloom, EffectComposer, ToneMapping } from '@react-three/postprocessing';
+import { Object3D, Vector3 } from 'three';
 import { Go2Robot } from './component/go2-robot';
 import { RobotCommunication } from './service/robot-communication.service';
 
 const connection = new RobotCommunication('ws://10.1.1.145:8765');
 
 const App = () => {
+  Object3D.DEFAULT_UP = new Vector3(0, 0, 1);
+
   return (
     <>
-      <Canvas>
-        <ambientLight intensity={Math.PI / 2} />
-        <Grid
-          infiniteGrid
-          renderOrder={-1}
-          position={[0, -0.45, 0]}
-          cellSize={0.45}
-          cellThickness={0.6}
-          sectionSize={0.45 * 8}
-          sectionThickness={2}
-          sectionColor={'teal'}
-          fadeDistance={50}
-        />
-        <OrbitControls enableDamping={true} enableZoom={true} makeDefault minPolarAngle={Math.PI / 4} maxPolarAngle={Math.PI / 2} />
-        <Go2Robot connection={connection} />
+      <Canvas shadows camera={{ position: [-4.0, 4.0, 4.0], fov: 65 }}>
+        <scene>
+          <Go2Robot connection={connection} />
+
+          <mesh receiveShadow>
+            <planeGeometry args={[10, 10]} />
+            <meshStandardMaterial />
+          </mesh>
+
+          <directionalLight castShadow position={[1, 1, 2]} rotation={[0, -0.5, 0]} color={0xeeeeff} intensity={2} shadow-radius={5} />
+
+          <group receiveShadow rotation={[3.14 / 2, 0.0, 0.0, 'ZYX']} position={[0.0, 0.0, 0.045]}>
+            <Environment background preset="sunset" blur={0.5} />
+            <ambientLight intensity={0.5} />
+            <Grid
+              infiniteGrid
+              renderOrder={-1}
+              position={[0.0, 0.0, 0.045]}
+              cellSize={0.45}
+              cellThickness={0.6}
+              sectionSize={0.45 * 8}
+              sectionThickness={2}
+              sectionColor={'teal'}
+              fadeDistance={50}
+            />
+            <EffectComposer>
+              <Bloom luminanceThreshold={2} mipmapBlur />
+              <ToneMapping />
+            </EffectComposer>
+          </group>
+        </scene>
+
+        <OrbitControls enableDamping={true} enableZoom={true} makeDefault minPolarAngle={Math.PI / 6} maxPolarAngle={Math.PI / 2} />
         <Stats />
       </Canvas>
     </>
--- src/component/go2-robot.tsx
+++ src/component/go2-robot.tsx
@@ -1,11 +1,10 @@
 import { useFrame, useLoader } from '@react-three/fiber';
-import { useEffect } from 'react';
 import { JointState, Odometry, Transform } from 'src/model/unitree-go2.model';
 import { RobotCommunication } from 'src/service/robot-communication.service';
-import URDFLoader from 'urdf-loader';
+import URDFLoader, { URDFRobot } from 'urdf-loader';
 import { TOPIC_JOINT_STATES, TOPIC_ODOM, TOPIC_TRANSFORM } from '../model/channels.model';
 
-const updateJoints = (connection: RobotCommunication, mesh: any) => {
+const updateJoints = (connection: RobotCommunication, mesh: URDFRobot) => {
   const jointState = connection.channelByName[TOPIC_JOINT_STATES]?.lastMessage as JointState;
   if (jointState && jointState.name) {
     const joints = jointState?.name.reduce((acc, joint, idx) => {
@@ -14,36 +13,30 @@ const updateJoints = (connection: RobotCommunication, mesh: any) => {
     }, {} as Record<string, number>);
 
     Object.keys(joints).forEach((joint) => {
-      mesh.joints[joint].setJointValue(joints![joint]);
+      mesh.joints[joint]!.setJointValue(joints[joint]!);
     });
   }
 };
 
-const updatePosition = (connection: RobotCommunication, mesh: any) => {
+const updatePosition = (connection: RobotCommunication, mesh: URDFRobot) => {
   const odom = connection.channelByName[TOPIC_ODOM]?.lastMessage as Odometry;
   if (odom && odom.pose) {
     mesh.position.copy(odom.pose.pose.position);
   }
 };
 
-const updateRotation = (connection: RobotCommunication, mesh: any) => {
+const updateRotation = (connection: RobotCommunication, mesh: URDFRobot) => {
   const transform = connection.channelByName[TOPIC_TRANSFORM]?.lastMessage as Transform;
   if (transform.transforms) {
     const base = transform.transforms.find((t) => t.child_frame_id === 'base_link');
     if (base) {
       mesh.quaternion.copy(base?.transform.rotation);
     }
-
   }
 };
 
-export const Go2Robot = (props: { connection: RobotCommunication; joints?: Record<string, number> }) => {
-  const robotMesh = useLoader(URDFLoader as any, '/assets/go2.urdf');
-
-  useEffect(() => {
-    robotMesh.eulerOrder = 'YXZ';
-    robotMesh.rotateX(Math.PI / -4);
-  }, []);
+export const Go2Robot = (props: { connection: RobotCommunication }) => {
+  const robotMesh = useLoader(URDFLoader as any, '/assets/go2.urdf') as URDFRobot;
 
   useFrame(() => {
     updateJoints(props.connection, robotMesh);
@@ -51,11 +44,9 @@ export const Go2Robot = (props: { connection: RobotCommunication; joints?: Recor
     updateRotation(props.connection, robotMesh);
   });
 
-  if (props.joints) {
-    Object.keys(props.joints).forEach((joint) => {
-      robotMesh.joints[joint].setJointValue(props.joints![joint]);
-    });
-  }
-
-  return <primitive object={robotMesh} />;
+  return (
+    <mesh castShadow>
+      <primitive object={robotMesh} />
+    </mesh>
+  );
 };
