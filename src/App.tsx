878df0b9b7fe2fe5904d01637edea0df979fa431
deejayy <deejayy@deejayy.hu>
2025-02-12 18:27:51 +0100

    Pointlight

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,41 @@
-import { ContactShadows, Environment, Grid, OrbitControls, Stats } from '@react-three/drei';
+import { Environment, Grid, OrbitControls, Stats } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Bloom, EffectComposer, ToneMapping } from '@react-three/postprocessing';
 import { Go2Robot } from './component/go2-robot';
 import { RobotCommunication } from './service/robot-communication.service';
-import { useRef } from 'react';
-import { DirectionalLight } from 'three';
 
 const connection = new RobotCommunication('ws://10.1.1.145:8765');
 
 const App = () => {
-  const lh = useRef(new DirectionalLight());
-
   return (
     <>
       <Canvas shadows camera={{ position: [-4.0, 4.0, 4.0], fov: 65 }}>
-        <group castShadow receiveShadow rotation={[-3.14 / 2, 0.0, 0.0, 'ZYX']} position={[0.0, -0.04, 0.0]}>
-          <mesh castShadow receiveShadow>
+        <group rotation={[-3.14 / 2, 0.0, 0.0, 'ZYX']} position={[0.0, -0.04, 0.0]}>
+          <mesh castShadow>
             <Go2Robot connection={connection} />
           </mesh>
-
-          <mesh receiveShadow>
+          <mesh receiveShadow position={[0.0, 0.0, 0.0]}>
             <planeGeometry args={[10, 10]} isBufferGeometry />
-            <meshStandardMaterial />
+            <meshLambertMaterial />
           </mesh>
         </group>
 
-        <directionalLight
+        <Environment background preset="sunset" blur={0.5} />
+        <ambientLight intensity={0.5} />
+
+        <pointLight
+          castShadow
           position={[2, 1, 2]}
           rotation={[0.0, -0.5, 0.5, 'XYZ']}
           color={0xeeeeff}
-          intensity={3}
-          shadow-radius={10}
-          ref={lh}
+          intensity={30}
+          shadow-radius={100}
+          shadow-mapSize-width={512}
+          shadow-mapSize-height={512}
+          shadow-camera-near={0.5}
+          shadow-camera-far={500}
         />
-        {/* <directionalLightHelper args={[lh.current, 1, 'red']} color={'red'} />
-        <ContactShadows blur={0.8} opacity={0.9} far={10} /> */}
 
-        <Environment background preset="sunset" blur={0.5} />
-        <ambientLight intensity={0.5} />
         <Grid
           infiniteGrid
           renderOrder={-1}
