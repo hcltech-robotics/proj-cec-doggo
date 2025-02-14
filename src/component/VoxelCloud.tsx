4989dcc52af952a244ac00d783555bf1ad37a704
deejayy <deejayy@deejayy.hu>
2025-02-14 15:07:29 +0100

    Downgrade to r18

--- src/component/VoxelCloud.tsx
+++ src/component/VoxelCloud.tsx
@@ -1,8 +1,8 @@
 import { useFrame, useLoader } from '@react-three/fiber';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { LidarData } from 'src/model/Go2RobotInterfaces';
 import { RobotCommunication } from 'src/service/RobotCommunicationService';
-import { BufferGeometry, DoubleSide, NearestFilter, TextureLoader } from 'three';
+import { DoubleSide, NearestFilter, TextureLoader } from 'three';
 import { topicList } from '../model/Go2RobotTopics';
 
 export const VoxelCloud = (props: { connection: RobotCommunication }) => {
@@ -12,24 +12,19 @@ export const VoxelCloud = (props: { connection: RobotCommunication }) => {
 
   const [data, setData] = useState<LidarData | undefined>(undefined);
 
-  const geometry = useRef<BufferGeometry>(undefined);
-
   useFrame(() => {
     setData(props.connection.channelByName[topicList.TOPIC_LIDAR]?.lastMessage);
-    if (geometry) {
-      geometry.current?.dispose();
-    }
   });
 
   return (
     <>
       {data?.geometryData ? (
-        <group rotation={[-3.14 / 2, 0.0, 0.0, 'ZYX']} position={[0.0, 0.0, 0.0]}>
+        <group rotation={[-3.14 / 2, 0.0, 0.0, 'ZYX']} position={[data.origin.at(0)!, data.origin.at(2)!, -1 * data.origin.at(1)!]}>
           <mesh scale={data?.resolution ?? 0.05} receiveShadow>
-            <bufferGeometry ref={geometry}>
-              <bufferAttribute args={[data.geometryData.positions || [], 3]} attach="attributes-position" needsUpdate />
-              <bufferAttribute args={[data.geometryData.uvs || [], 2, true]} attach="attributes-uv" needsUpdate />
-              <bufferAttribute args={[data.geometryData.indices || [], 1]} attach="index" needsUpdate />
+            <bufferGeometry>
+              <bufferAttribute args={[data.geometryData.positions || [], 3]} attach="attributes-position" />
+              <bufferAttribute args={[data.geometryData.uvs || [], 2, true]} attach="attributes-uv" />
+              <bufferAttribute args={[data.geometryData.indices || [], 1]} attach="index" />
             </bufferGeometry>
             <meshStandardMaterial side={DoubleSide} map={voxelTex} transparent={false} vertexColors />
           </mesh>
