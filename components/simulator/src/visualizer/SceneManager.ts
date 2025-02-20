f5ab851991a54aa6610eb0c26acf2a6584ce8f77
Istvan Ivanyi <ivanyii@starschema.net>
2025-02-20 23:33:33 +0100

    format

--- components/simulator/src/visualizer/SceneManager.ts
+++ components/simulator/src/visualizer/SceneManager.ts
@@ -1,128 +1,132 @@
+import { Clock, Vector3, WebGLRenderer } from 'three';
+import { FoxgloveClient } from '@foxglove/ws-protocol';
+import { createFoxGloveWebsocket, WebSocketEventHandler } from '../robot/foxgloveConnection';
+import { initThreeJSBase } from './views/initThreeJs';
+import { initLidarWebWorker } from './views/lidarBox/lidarBoxTransformation';
+import { transform_cb } from './transformations/ros2transforms';
+import { animate } from './renderloop';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
 import {
-    Clock,
-    Vector3,
-    WebGLRenderer,
-} from 'three'
-import { FoxgloveClient } from '@foxglove/ws-protocol'
-import { createFoxGloveWebsocket, WebSocketEventHandler } from '../robot/foxgloveConnection'
-import { initThreeJSBase } from './views/initThreeJs'
-import { initLidarWebWorker } from './views/lidarBox/lidarBoxTransformation'
-import { transform_cb } from './transformations/ros2transforms'
-import { animate } from './renderloop'
-import Stats from 'three/examples/jsm/libs/stats.module.js'
-import { CameraDepthScene, CameraDepthSceneUserData, MainScene, PointcloudScene, PointcloudSceneUserData, Scenes, UserSettings } from './types'
-import { Channel } from '@foxglove/ws-protocol'
+  CameraDepthScene,
+  CameraDepthSceneUserData,
+  MainScene,
+  PointcloudScene,
+  PointcloudSceneUserData,
+  Scenes,
+  UserSettings,
+} from './types';
+import { Channel } from '@foxglove/ws-protocol';
 
-const CANVAS_ID = 'scene'
+const CANVAS_ID = 'scene';
 
 class SceneManager {
-    canvas: HTMLElement
-    renderer: WebGLRenderer | null
-    clock: Clock
-    stats: Stats
-    scenes: Scenes
-    userSettings: UserSettings
-    client: FoxgloveClient | null
-    foxgloveConnection: Promise<void>
-    eventTarget: EventTarget
+  canvas: HTMLElement;
+  renderer: WebGLRenderer | null;
+  clock: Clock;
+  stats: Stats;
+  scenes: Scenes;
+  userSettings: UserSettings;
+  client: FoxgloveClient | null;
+  foxgloveConnection: Promise<void>;
+  eventTarget: EventTarget;
 
-    get miniScenes() {
-      return [this.scenes.pointcloud, this.scenes.cameraDepth];
-    }
+  get miniScenes() {
+    return [this.scenes.pointcloud, this.scenes.cameraDepth];
+  }
 
-    private animationFrame: number
+  private animationFrame: number;
 
-    constructor() {
-        this.userSettings = {
-            animation: { enabled: true, play: true },
-            apiKey: 'defaultapiKey',
-            foxglove_config: { url: "ws://localhost:8765" },
-            pointCloudScene: { enabled: true },
-            cameraDepthScene: { enabled: true },
-            topics: ['No Point Cloud Scene'],
-            selectedMiniScene: '',
-        };
-        this.scenes = {
-            main: new MainScene(),
-            pointcloud: new PointcloudScene(),
-            cameraDepth: new CameraDepthScene(),
-        }
-        this.canvas = document.querySelector<HTMLElement>(`canvas#${CANVAS_ID}`)!
-        this.renderer = null
-        this.client = null
-        this.clock = new Clock()
-        this.stats = new Stats()
-        this.foxgloveConnection = Promise.resolve()
-        this.animationFrame = 0;
-        this.eventTarget = new EventTarget();
-    }
+  constructor() {
+    this.userSettings = {
+      animation: { enabled: true, play: true },
+      apiKey: 'defaultapiKey',
+      foxglove_config: { url: 'ws://localhost:8765' },
+      pointCloudScene: { enabled: true },
+      cameraDepthScene: { enabled: true },
+      topics: ['No Point Cloud Scene'],
+      selectedMiniScene: '',
+    };
+    this.scenes = {
+      main: new MainScene(),
+      pointcloud: new PointcloudScene(),
+      cameraDepth: new CameraDepthScene(),
+    };
+    this.canvas = document.querySelector<HTMLElement>(`canvas#${CANVAS_ID}`)!;
+    this.renderer = null;
+    this.client = null;
+    this.clock = new Clock();
+    this.stats = new Stats();
+    this.foxgloveConnection = Promise.resolve();
+    this.animationFrame = 0;
+    this.eventTarget = new EventTarget();
+  }
 
-    parseChannels(channels: Channel[]) {
-      this.userSettings.topics = channels.reduce((acc, curr) => {
-        if (curr.schemaName.includes('PointCloud2')) {
-          acc.push(curr.topic);
-        }
-        return acc;
-      }, this.userSettings.topics);
-      this.eventTarget.dispatchEvent(new CustomEvent('topicsloaded', { detail: this.userSettings.topics }));
-    }
-
-    init(onEvent: WebSocketEventHandler) {
-        initThreeJSBase(this, onEvent)
-        createFoxGloveWebsocket(transform_cb, this.userSettings.foxglove_config.url, this, onEvent);
-        initLidarWebWorker(this)
-        this.bindEventListeners();
-    }
-
-    reconnectWebsocketConnection(onEvent: WebSocketEventHandler) {
-        if (this.client) {
-          this.client.close();
-          this.client = null;
-        }
-
-        createFoxGloveWebsocket(transform_cb, this.userSettings.foxglove_config.url, this, onEvent);
-    }
-
-    animate() {
-        animate(this)
-    }
-
-    bindEventListeners() {
-      this.miniScenes.forEach(({ userData }) => {
-        userData.cameraControls?.addEventListener('start', () => {
-          cancelAnimationFrame(this.animationFrame);
-        });
-
-        userData.domElement?.addEventListener('mouseleave', () => {
-          this.resetCameraPosition(userData);
-        });
-      });
-    }
-
-    resetCameraPosition({ camera, cameraControls, resetPosition, lerpSpeed }: PointcloudSceneUserData | CameraDepthSceneUserData) {
-      if (!camera || !cameraControls) {
-        return;
+  parseChannels(channels: Channel[]) {
+    this.userSettings.topics = channels.reduce((acc, curr) => {
+      if (curr.schemaName.includes('PointCloud2')) {
+        acc.push(curr.topic);
       }
+      return acc;
+    }, this.userSettings.topics);
+    this.eventTarget.dispatchEvent(new CustomEvent('topicsloaded', { detail: this.userSettings.topics }));
+  }
 
-      const update = () => {
-        if (camera.position.distanceTo(resetPosition) > 0.0001) {
-          camera.position.lerp(resetPosition, lerpSpeed);
-          cameraControls.target.lerp(new Vector3(0, 0, 0), lerpSpeed);
-          camera.lookAt(0, 0, 0);
-          camera.updateProjectionMatrix();
-          cameraControls.update();
+  init(onEvent: WebSocketEventHandler) {
+    initThreeJSBase(this, onEvent);
+    createFoxGloveWebsocket(transform_cb, this.userSettings.foxglove_config.url, this, onEvent);
+    initLidarWebWorker(this);
+    this.bindEventListeners();
+  }
 
-          this.animationFrame = requestAnimationFrame(update);
-        } else {
-          cancelAnimationFrame(this.animationFrame);
-        }
-      };
-
-      update();
+  reconnectWebsocketConnection(onEvent: WebSocketEventHandler) {
+    if (this.client) {
+      this.client.close();
+      this.client = null;
     }
+
+    createFoxGloveWebsocket(transform_cb, this.userSettings.foxglove_config.url, this, onEvent);
+  }
+
+  animate() {
+    animate(this);
+  }
+
+  bindEventListeners() {
+    this.miniScenes.forEach(({ userData }) => {
+      userData.cameraControls?.addEventListener('start', () => {
+        cancelAnimationFrame(this.animationFrame);
+      });
+
+      userData.domElement?.addEventListener('mouseleave', () => {
+        this.resetCameraPosition(userData);
+      });
+    });
+  }
+
+  resetCameraPosition({ camera, cameraControls, resetPosition, lerpSpeed }: PointcloudSceneUserData | CameraDepthSceneUserData) {
+    if (!camera || !cameraControls) {
+      return;
+    }
+
+    const update = () => {
+      if (camera.position.distanceTo(resetPosition) > 0.0001) {
+        camera.position.lerp(resetPosition, lerpSpeed);
+        cameraControls.target.lerp(new Vector3(0, 0, 0), lerpSpeed);
+        camera.lookAt(0, 0, 0);
+        camera.updateProjectionMatrix();
+        cameraControls.update();
+
+        this.animationFrame = requestAnimationFrame(update);
+      } else {
+        cancelAnimationFrame(this.animationFrame);
+      }
+    };
+
+    update();
+  }
 }
 
 const sceneManager: SceneManager = new SceneManager();
 const getSceneManager = () => sceneManager;
 
-export { type SceneManager, getSceneManager }
+export { type SceneManager, getSceneManager };
--- components/simulator/src/visualizer/types.ts
+++ components/simulator/src/visualizer/types.ts
@@ -1,87 +1,100 @@
 import {
-    AmbientLight,
-    AxesHelper,
-    GridHelper,
-    Material,
-    PerspectiveCamera,
-    PointLight,
-    PointLightHelper,
-    Points,
-    Scene,
-    Vector3,
-} from 'three'
-import { DragControls, OrbitControls } from 'three/examples/jsm/Addons.js'
-import { URDFRobot } from 'urdf-loader'
+  AmbientLight,
+  AxesHelper,
+  GridHelper,
+  Material,
+  PerspectiveCamera,
+  PointLight,
+  PointLightHelper,
+  Points,
+  Scene,
+  Vector3,
+} from 'three';
+import { DragControls, OrbitControls } from 'three/examples/jsm/Addons.js';
+import { URDFRobot } from 'urdf-loader';
 
 interface MainSceneUserData {
-    camera: PerspectiveCamera | null
-    lidarWebWorker: Worker | null
-    ambientLight: AmbientLight | null
-    pointLight: PointLight | null
-    lidarMaterial: Material | null
-    gridHelper: GridHelper | null
-    cameraControls: OrbitControls | null
-    dragControls: DragControls | null
-    axesHelper: AxesHelper | null
-    pointLightHelper: PointLightHelper | null
-    pointsCloud: Points | null
-    robot: URDFRobot | null
-    domElement: HTMLElement | null
+  camera: PerspectiveCamera | null;
+  lidarWebWorker: Worker | null;
+  ambientLight: AmbientLight | null;
+  pointLight: PointLight | null;
+  lidarMaterial: Material | null;
+  gridHelper: GridHelper | null;
+  cameraControls: OrbitControls | null;
+  dragControls: DragControls | null;
+  axesHelper: AxesHelper | null;
+  pointLightHelper: PointLightHelper | null;
+  pointsCloud: Points | null;
+  robot: URDFRobot | null;
+  domElement: HTMLElement | null;
 }
 
 export class MainScene extends Scene {
-    userData: MainSceneUserData
-    constructor() {
-        super()
-        this.userData = { lidarWebWorker: null, camera: null, ambientLight: null, pointLight: null, lidarMaterial: null, cameraControls: null, dragControls: null, axesHelper: null, pointLightHelper: null, pointsCloud: null, robot: null, domElement: null, gridHelper: null }
-    }
+  userData: MainSceneUserData;
+  constructor() {
+    super();
+    this.userData = {
+      lidarWebWorker: null,
+      camera: null,
+      ambientLight: null,
+      pointLight: null,
+      lidarMaterial: null,
+      cameraControls: null,
+      dragControls: null,
+      axesHelper: null,
+      pointLightHelper: null,
+      pointsCloud: null,
+      robot: null,
+      domElement: null,
+      gridHelper: null,
+    };
+  }
 }
 
 export interface PointcloudSceneUserData {
-    camera: PerspectiveCamera | null
-    domElement: HTMLElement | null
-    cameraControls: OrbitControls | null
-    resetPosition: Vector3
-    lerpSpeed: number
+  camera: PerspectiveCamera | null;
+  domElement: HTMLElement | null;
+  cameraControls: OrbitControls | null;
+  resetPosition: Vector3;
+  lerpSpeed: number;
 }
 
 export class PointcloudScene extends Scene {
-    userData: PointcloudSceneUserData
-    constructor() {
-        super()
-        this.userData = { camera: null, domElement: null, cameraControls: null, resetPosition: new Vector3(0, 1, 0), lerpSpeed: 0.05 }
-    }
+  userData: PointcloudSceneUserData;
+  constructor() {
+    super();
+    this.userData = { camera: null, domElement: null, cameraControls: null, resetPosition: new Vector3(0, 1, 0), lerpSpeed: 0.05 };
+  }
 }
 
 export interface CameraDepthSceneUserData {
-  camera: PerspectiveCamera | null
-  domElement: HTMLElement | null
-  cameraControls: OrbitControls | null
-  resetPosition: Vector3
-  lerpSpeed: number
+  camera: PerspectiveCamera | null;
+  domElement: HTMLElement | null;
+  cameraControls: OrbitControls | null;
+  resetPosition: Vector3;
+  lerpSpeed: number;
 }
 
 export class CameraDepthScene extends Scene {
-  userData: CameraDepthSceneUserData
+  userData: CameraDepthSceneUserData;
   constructor() {
-      super()
-      this.userData = { camera: null, domElement: null, cameraControls: null, resetPosition: new Vector3(0, 1, 0), lerpSpeed: 0.1 }
+    super();
+    this.userData = { camera: null, domElement: null, cameraControls: null, resetPosition: new Vector3(0, 1, 0), lerpSpeed: 0.1 };
   }
 }
 
 export interface Scenes {
-    main: MainScene,
-    pointcloud: PointcloudScene,
-    cameraDepth: CameraDepthScene,
+  main: MainScene;
+  pointcloud: PointcloudScene;
+  cameraDepth: CameraDepthScene;
 }
 
 export interface UserSettings {
-    animation: { enabled: boolean, play: boolean }
-    apiKey: string
-    foxglove_config: { url: string }
-    pointCloudScene: { enabled: boolean }
-    cameraDepthScene: { enabled: boolean }
-    selectedMiniScene: string
-    topics: string[]
+  animation: { enabled: boolean; play: boolean };
+  apiKey: string;
+  foxglove_config: { url: string };
+  pointCloudScene: { enabled: boolean };
+  cameraDepthScene: { enabled: boolean };
+  selectedMiniScene: string;
+  topics: string[];
 }
-
