5d1d442c643f9f442d8cd305bf491c801f414c45
Istvan Ivanyi <ivanyii@starschema.net>
2025-02-24 18:15:23 +0100

    adjust urdf loader packages

--- components/simulator/src/visualizer/robot/robotLoader.ts
+++ components/simulator/src/visualizer/robot/robotLoader.ts
@@ -22,7 +22,9 @@ function createRobot(s: SceneManager, content: string) {
   const loader = new URDFLoader(manager);
 
   loader.packages = {
+    "go2_robot_sdk": import.meta.env.BASE_URL + "go2_robot_sdk",
     "go2_description": import.meta.env.BASE_URL + "go2_robot_sdk",
+    "realsense2_description": import.meta.env.BASE_URL + "go2_robot_sdk",
   };
 
   const robot = loader.parse(content).translateZ(0.45);
